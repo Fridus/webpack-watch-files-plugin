34ec0d7e72fdcf699ac664ecc1640324f57cdade
Florent Detry <florent@igloo.be>
2020-08-31 10:14:34 +0200

    Update dependencies

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -3,6 +3,6 @@ import path from 'path'
 
 export default {
   context: process.cwd(),
-  entry: [ './test/main.js' ],
+  entry: ['./test/main.js'],
   output: { path: path.join(__dirname, 'output') }
 }
